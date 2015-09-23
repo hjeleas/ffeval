0286d133eb1ed827f2650636d7dd20f2e7c3bb7d
hjeleas <hjeleas@hotmail.com>
2015-09-23 16:10:11 +0200

    initial

--- ffeval/main.cpp
+++ ffeval/main.cpp
@@ -15,20 +15,21 @@
 
 
 #define VERSION "0.1"
-#define HELP "Use: ffeval [options] <filename> offset:length \"noise-model\"\n"\
+#define HELP "Use: ffeval <filename> offset:length \"structure\"\n"\
 			"Version " VERSION  "\n" \
-            "Offset and length in bytes\nOptions:\n" \
-            " -b : binary \n -a : ascii \n -h : hex (default)\n -w \"STRUCT-STRING\": audio file (.wav)\n -i \"STRUCT-STRING\" : image file (.bmp) -h \"STRUCT-STRING\"  : histogram plot (.csv)\n" \
-            "\n\"STRUCT-STRING\", for example:\n* 'BYTE' : sequence of bytes \n* 'UNSIGNED,byte': sequence of unsigned integer followed by one ignored byte.\n" \
-            "\n\"NOISE-MODEL\" : eg.\n* 'BSR=0.5' : 50%% bit flip"
+            "Structure: eg. 'float' : array of floats"
+            
 
 #define NBUF 1024
-unsigned char *buf[NBUF];
+#define NASC 100
+unsigned char buf[NBUF];
+unsigned char primative[NBUF];
+
 
 
 int main(int argc, char **argv)
 {
-	char tmp[1024];
+	char tmp[1024],*structstring="c";
 	if (argc == 1) {
 		fprintf(stderr, HELP);
 		exit(1);
@@ -43,15 +44,55 @@ int main(int argc, char **argv)
 		char *sp = strtok(tmp, ":");  offset = atoi(sp);
 		sp = strtok(NULL, ":");  length = atoi(sp);
 	}
-	fprintf(stderr, "File: %s offset %d length %d\n", fn, offset, length);
+	if (argc == 4) {
+		structstring =  argv[3];
+	}
+	fprintf(stderr, "File: %s\nOffset %d length %d structure '%s'\n", fn, offset, length, structstring);
 	FILE *fin = fopen(fn, "rb");
 	if (!fin) { fprintf(stderr, "cannot open file %s\n", fn); exit(1); }
 	fseek(fin, offset, SEEK_SET);
-	int l = 0;
-	do {
-		l = fread(buf, 1, NBUF, fin);
+	
+	char asc[NASC] = "";	
+	unsigned char tmps[NASC]; memset(tmps, 0x00, NASC);
+	int origprimsize = 0,primsize =0, lenstruct = strlen(structstring),k=0,kk=0;
+	char sitem = '^';
+	int  wiggle = 0;
+	unsigned char c = '\0';
+	for (int l = NBUF, i = 0; length > 0 && l == (NBUF-wiggle); i += l) {
+		printf("\n*** file boundary %d length %d\n", i, NBUF - wiggle)
+		l = fread(buf+wiggle, 1, NBUF-wiggle, fin);
 		length -= l;
-	} while (length > 0 && l==NBUF);
+
+
+		for (int j = 0 ; j < l; ++j) {
+			c = ((unsigned char *)buf)[j];
+
+			if (primsize == 0) {
+
+				if (sitem != '^') { /* ignore the first pass*/
+
+					switch (sitem) {
+					case 'c': { asc[0] = (isprint(((unsigned char*)tmps)[0])) ? (((unsigned char*)tmps)[0]) : '.'; asc[1] = '\0';   break; }   /* byte = single character */
+					case 'C': { for (kk = 0; kk < 16; ++kk) asc[15-kk] = (isprint(((unsigned char*)tmps)[kk])) ? (((unsigned char*)tmps)[kk]) : '.'; asc[16] = '\0';  break; }  /* per 16 bytes character */
+					}
+					for (kk = origprimsize-1; kk >=0 ; --kk) printf("%02x ", tmps[kk]);
+				}
+				printf("  %s\n%08d: ", asc, j);  memset(asc, 0x00, NASC);
+
+
+				sitem = structstring[k%lenstruct]; k++;
+				switch (sitem) {
+				 case 'c': { origprimsize=primsize = 1;  break; }   /* byte = single character */
+				 case 'C': { origprimsize=primsize = 16;  break; }  /* per 16 bytes character */
+				 default: { fprintf(stderr, "-- Unknown structure specification '%c'\n", sitem); exit(1);  }
+				}
+				
+			}
+
+			tmps[--primsize] = c;
+
+	    }   /* through readed buffer*/
+	}   /* entire file per buffer of size nbuf (largest size smaller than NBUF and multiple of sum of primitive of lenstruct*/
 
 	fclose(fin);
 
